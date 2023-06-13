3dc9c30426b0c6fe1b67f581a8663af45729203f
path71nder <sevillebayuaji@gmail.com>
2023-06-13 11:29:47 +0700

    Session controller fixed

---
+++ controllers/sessionController.js
@@ -0,0 +1,23 @@
+const userModel = require('../models/userModel');
+
+exports.getSessionList = async (req, res) => {
+    const { user, active } = req.body;
+    try {
+        const sessionList = await userModel.getSessionList(user, active);
+        res.json(sessionList);
+    } catch (err) {
+        res.send({ error: err.message });
+    }
+};
+
+exports.deleteSession = async (req, res) => {
+    const { id } = req.body;
+    try {
+        const sessionDelete = await userModel.deleteSession(id);
+        res.send({
+            status: 'Session has been deleted'
+        });
+    } catch (err) {
+        res.status(500).send({ error: err.message });
+    }
+};
